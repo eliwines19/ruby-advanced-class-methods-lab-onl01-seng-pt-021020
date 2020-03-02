dad61772b683c3c0022bbee277f1d39fd7fba009
Elijah wines <elijahwines84@yahoo.com>
2020-03-02 17:40:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,5 +45,15 @@ class Song
     self.all.sort_by {|song| song.name[0]}
   end
 
+  def self.new_from_filename(filename)
+    song_data = filename.sub!(/.mp3/,'').split(" - ")
+
+    song = self.new 
+    song.artist_name = song_data[0]
+    song.name = song_data[1]
+
+    song
+  end
+
   
 end
