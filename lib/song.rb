cef49b8f36b0c7f0a98215ecc0423aa6a3818d3f
Elijah wines <elijahwines84@yahoo.com>
2020-03-02 17:42:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -55,5 +55,12 @@ class Song
     song
   end
 
-  
+  def self.create_from_filename(filename)
+    song_data = filename.sub!(/.mp3/,'').split(" - ")
+
+    song = self.create
+    song.artist_name = song_data[0]
+    song.name = song_data[1]
+
+    song
 end
